4b496e9eaac6bd9804c9a3059e4efc765367ab3f
Kevin Kelley <kelleyk@kelleyk.net>
2016-10-22 13:05:01 -0700

    Add additional constants; remove unnecessary typing; regenerate stringers.

--- packet.go
+++ packet.go
@@ -195,6 +195,8 @@ func (p *Packet) PadToMinSize() {
 	}
 }
 
+// N.B.: All of these constants are described here: https://www.iana.org/assignments/bootp-dhcp-parameters/bootp-dhcp-parameters.xhtml
+
 // OpCodes
 const (
 	BootRequest OpCode = 1 // From Client
@@ -205,14 +207,24 @@ const (
 
 // DHCP Message Type 53
 const (
-	Discover MessageType = 1 // Broadcast Packet From Client - Can I have an IP?
-	Offer    MessageType = 2 // Broadcast From Server - Here's an IP
-	Request  MessageType = 3 // Broadcast From Client - I'll take that IP (Also start for renewals)
-	Decline  MessageType = 4 // Broadcast From Client - Sorry I can't use that IP
-	ACK      MessageType = 5 // From Server, Yes you can have that IP
-	NAK      MessageType = 6 // From Server, No you cannot have that IP
-	Release  MessageType = 7 // From Client, I don't need that IP anymore
-	Inform   MessageType = 8 // From Client, I have this IP and there's nothing you can do about it
+	Discover         MessageType = 1 // Broadcast Packet From Client - Can I have an IP?
+	Offer                        = 2 // Broadcast From Server - Here's an IP
+	Request                      = 3 // Broadcast From Client - I'll take that IP (Also start for renewals)
+	Decline                      = 4 // Broadcast From Client - Sorry I can't use that IP
+	ACK                          = 5 // From Server, Yes you can have that IP
+	NAK                          = 6 // From Server, No you cannot have that IP
+	Release                      = 7 // From Client, I don't need that IP anymore
+	Inform                       = 8 // From Client, I have this IP and there's nothing you can do about it
+	ForceRenew                   = 9
+	LeaseQuery                   = 10
+	LeaseUnassigned              = 11
+	LeaseUnknown                 = 12
+	LeaseActive                  = 13
+	BulkLeaseQuery               = 14
+	LeaseQueryDone               = 15
+	ActiveLeaseQuery             = 16
+	LeaseQueryStatus             = 17
+	TLS                          = 18
 )
 
 //go:generate stringer -type=OptionCode
@@ -220,105 +232,131 @@ const (
 // DHCP Options
 const (
 	End                          OptionCode = 255
-	Pad                          OptionCode = 0
-	OptionSubnetMask             OptionCode = 1
-	OptionTimeOffset             OptionCode = 2
-	OptionRouter                 OptionCode = 3
-	OptionTimeServer             OptionCode = 4
-	OptionNameServer             OptionCode = 5
-	OptionDomainNameServer       OptionCode = 6
-	OptionLogServer              OptionCode = 7
-	OptionCookieServer           OptionCode = 8
-	OptionLPRServer              OptionCode = 9
-	OptionImpressServer          OptionCode = 10
-	OptionResourceLocationServer OptionCode = 11
-	OptionHostName               OptionCode = 12
-	OptionBootFileSize           OptionCode = 13
-	OptionMeritDumpFile          OptionCode = 14
-	OptionDomainName             OptionCode = 15
-	OptionSwapServer             OptionCode = 16
-	OptionRootPath               OptionCode = 17
-	OptionExtensionsPath         OptionCode = 18
+	Pad                                     = 0
+	OptionSubnetMask                        = 1
+	OptionTimeOffset                        = 2
+	OptionRouter                            = 3
+	OptionTimeServer                        = 4
+	OptionNameServer                        = 5
+	OptionDomainNameServer                  = 6
+	OptionLogServer                         = 7
+	OptionCookieServer                      = 8
+	OptionLPRServer                         = 9
+	OptionImpressServer                     = 10
+	OptionResourceLocationServer            = 11
+	OptionHostName                          = 12
+	OptionBootFileSize                      = 13
+	OptionMeritDumpFile                     = 14
+	OptionDomainName                        = 15
+	OptionSwapServer                        = 16
+	OptionRootPath                          = 17
+	OptionExtensionsPath                    = 18
 
 	// IP Layer Parameters per Host
-	OptionIPForwardingEnableDisable          OptionCode = 19
-	OptionNonLocalSourceRoutingEnableDisable OptionCode = 20
-	OptionPolicyFilter                       OptionCode = 21
-	OptionMaximumDatagramReassemblySize      OptionCode = 22
-	OptionDefaultIPTimeToLive                OptionCode = 23
-	OptionPathMTUAgingTimeout                OptionCode = 24
-	OptionPathMTUPlateauTable                OptionCode = 25
+	OptionIPForwardingEnableDisable          = 19
+	OptionNonLocalSourceRoutingEnableDisable = 20
+	OptionPolicyFilter                       = 21
+	OptionMaximumDatagramReassemblySize      = 22
+	OptionDefaultIPTimeToLive                = 23
+	OptionPathMTUAgingTimeout                = 24
+	OptionPathMTUPlateauTable                = 25
 
 	// IP Layer Parameters per Interface
-	OptionInterfaceMTU              OptionCode = 26
-	OptionAllSubnetsAreLocal        OptionCode = 27
-	OptionBroadcastAddress          OptionCode = 28
-	OptionPerformMaskDiscovery      OptionCode = 29
-	OptionMaskSupplier              OptionCode = 30
-	OptionPerformRouterDiscovery    OptionCode = 31
-	OptionRouterSolicitationAddress OptionCode = 32
-	OptionStaticRoute               OptionCode = 33
+	OptionInterfaceMTU              = 26
+	OptionAllSubnetsAreLocal        = 27
+	OptionBroadcastAddress          = 28
+	OptionPerformMaskDiscovery      = 29
+	OptionMaskSupplier              = 30
+	OptionPerformRouterDiscovery    = 31
+	OptionRouterSolicitationAddress = 32
+	OptionStaticRoute               = 33
 
 	// Link Layer Parameters per Interface
-	OptionTrailerEncapsulation  OptionCode = 34
-	OptionARPCacheTimeout       OptionCode = 35
-	OptionEthernetEncapsulation OptionCode = 36
+	OptionTrailerEncapsulation  = 34
+	OptionARPCacheTimeout       = 35
+	OptionEthernetEncapsulation = 36
 
 	// TCP Parameters
-	OptionTCPDefaultTTL        OptionCode = 37
-	OptionTCPKeepaliveInterval OptionCode = 38
-	OptionTCPKeepaliveGarbage  OptionCode = 39
+	OptionTCPDefaultTTL        = 37
+	OptionTCPKeepaliveInterval = 38
+	OptionTCPKeepaliveGarbage  = 39
 
 	// Application and Service Parameters
-	OptionNetworkInformationServiceDomain            OptionCode = 40
-	OptionNetworkInformationServers                  OptionCode = 41
-	OptionNetworkTimeProtocolServers                 OptionCode = 42
-	OptionVendorSpecificInformation                  OptionCode = 43
-	OptionNetBIOSOverTCPIPNameServer                 OptionCode = 44
-	OptionNetBIOSOverTCPIPDatagramDistributionServer OptionCode = 45
-	OptionNetBIOSOverTCPIPNodeType                   OptionCode = 46
-	OptionNetBIOSOverTCPIPScope                      OptionCode = 47
-	OptionXWindowSystemFontServer                    OptionCode = 48
-	OptionXWindowSystemDisplayManager                OptionCode = 49
-	OptionNetworkInformationServicePlusDomain        OptionCode = 64
-	OptionNetworkInformationServicePlusServers       OptionCode = 65
-	OptionMobileIPHomeAgent                          OptionCode = 68
-	OptionSimpleMailTransportProtocol                OptionCode = 69
-	OptionPostOfficeProtocolServer                   OptionCode = 70
-	OptionNetworkNewsTransportProtocol               OptionCode = 71
-	OptionDefaultWorldWideWebServer                  OptionCode = 72
-	OptionDefaultFingerServer                        OptionCode = 73
-	OptionDefaultInternetRelayChatServer             OptionCode = 74
-	OptionStreetTalkServer                           OptionCode = 75
-	OptionStreetTalkDirectoryAssistance              OptionCode = 76
+	OptionNetworkInformationServiceDomain            = 40
+	OptionNetworkInformationServers                  = 41
+	OptionNetworkTimeProtocolServers                 = 42
+	OptionVendorSpecificInformation                  = 43
+	OptionNetBIOSOverTCPIPNameServer                 = 44
+	OptionNetBIOSOverTCPIPDatagramDistributionServer = 45
+	OptionNetBIOSOverTCPIPNodeType                   = 46
+	OptionNetBIOSOverTCPIPScope                      = 47
+	OptionXWindowSystemFontServer                    = 48
+	OptionXWindowSystemDisplayManager                = 49
+	OptionNetworkInformationServicePlusDomain        = 64
+	OptionNetworkInformationServicePlusServers       = 65
+	OptionMobileIPHomeAgent                          = 68
+	OptionSimpleMailTransportProtocol                = 69
+	OptionPostOfficeProtocolServer                   = 70
+	OptionNetworkNewsTransportProtocol               = 71
+	OptionDefaultWorldWideWebServer                  = 72
+	OptionDefaultFingerServer                        = 73
+	OptionDefaultInternetRelayChatServer             = 74
+	OptionStreetTalkServer                           = 75
+	OptionStreetTalkDirectoryAssistance              = 76
 
-	OptionRelayAgentInformation OptionCode = 82
+	OptionRelayAgentInformation = 82
 
 	// DHCP Extensions
-	OptionRequestedIPAddress     OptionCode = 50
-	OptionIPAddressLeaseTime     OptionCode = 51
-	OptionOverload               OptionCode = 52
-	OptionDHCPMessageType        OptionCode = 53
-	OptionServerIdentifier       OptionCode = 54
-	OptionParameterRequestList   OptionCode = 55
-	OptionMessage                OptionCode = 56
-	OptionMaximumDHCPMessageSize OptionCode = 57
-	OptionRenewalTimeValue       OptionCode = 58
-	OptionRebindingTimeValue     OptionCode = 59
-	OptionVendorClassIdentifier  OptionCode = 60
-	OptionClientIdentifier       OptionCode = 61
+	OptionRequestedIPAddress     = 50
+	OptionIPAddressLeaseTime     = 51
+	OptionOverload               = 52
+	OptionDHCPMessageType        = 53
+	OptionServerIdentifier       = 54
+	OptionParameterRequestList   = 55
+	OptionMessage                = 56
+	OptionMaximumDHCPMessageSize = 57
+	OptionRenewalTimeValue       = 58
+	OptionRebindingTimeValue     = 59
+	OptionVendorClassIdentifier  = 60
+	OptionClientIdentifier       = 61
 
-	OptionTFTPServerName OptionCode = 66
-	OptionBootFileName   OptionCode = 67
+	OptionTFTPServerName = 66
+	OptionBootFileName   = 67
 
-	OptionUserClass OptionCode = 77
+	OptionUserClass = 77
 
-	OptionClientArchitecture OptionCode = 93
+	OptionClientArchitecture = 93
 
-	OptionTZPOSIXString    OptionCode = 100
-	OptionTZDatabaseString OptionCode = 101
+	OptionTZPOSIXString    = 100
+	OptionTZDatabaseString = 101
 
-	OptionClasslessRouteFormat OptionCode = 121
+	OptionClasslessRouteFormat = 121
+
+	OptionNetWareIPDomain = 62
+	OptionNetWareIPOption = 63 // see sub-option codes specified in RFC2242
+
+	OptionDirectoryAgent = 78
+	OptionServiceScope   = 79
+	OptionRapidCommit    = 80
+	OptionClientFQDN     = 81
+
+	OptioniSNS = 83 // Internet Storage Name Service
+
+	OptionNDSServers                    = 85 // Novell Directory Services
+	OptionNDSTreeName                   = 86
+	OptionNDSContext                    = 87
+	OptionBCMCSControllerDomainNameList = 88
+	OptionBCMCSControllerIPv4Address    = 89
+	OptionAuthentication                = 90
+	OptionClientLastTransactionTime     = 91
+	OptionAssociatedIP                  = 92
+
+	OptionClientNDI = 94
+	OptionLDAP      = 95
+
+	OptionUUID         = 97
+	OptionUserAuth     = 98 // Open Group's User Authentication
+	OptionGeoconfCivic = 99
 )
 
 /* Notes
